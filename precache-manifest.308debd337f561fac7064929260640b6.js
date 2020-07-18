a39e33563f1a74c550e21dff9a6c0dffb72fbe9d
Tushant882637 <tushant858@gmail.com>
2020-07-18 13:54:33 +0530

    Updates

---
+++ precache-manifest.308debd337f561fac7064929260640b6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d7547cf7ba62907c160",
+    "url": "/ReactLive/static/css/main.b4a06325.chunk.css"
+  },
+  {
+    "revision": "5d7547cf7ba62907c160",
+    "url": "/ReactLive/static/js/main.eca3f93a.chunk.js"
+  },
+  {
+    "revision": "4a4a4ca3a2d9b013e6b6",
+    "url": "/ReactLive/static/js/runtime~main.07cc4cd0.js"
+  },
+  {
+    "revision": "1c2b1a489ad8397ed19c",
+    "url": "/ReactLive/static/css/2.08685f82.chunk.css"
+  },
+  {
+    "revision": "1c2b1a489ad8397ed19c",
+    "url": "/ReactLive/static/js/2.0f004c47.chunk.js"
+  },
+  {
+    "revision": "0223b85758d1fe68a2eda5e50259c738",
+    "url": "/ReactLive/static/media/banner-2.0223b857.png"
+  },
+  {
+    "revision": "d7013e3fae0a828b037741f9a187545a",
+    "url": "/ReactLive/static/media/project-6.d7013e3f.jpg"
+  },
+  {
+    "revision": "b72b11f5731bc13c3efe37cc78307ac8",
+    "url": "/ReactLive/static/media/project-5.b72b11f5.png"
+  },
+  {
+    "revision": "e54aabb44c3f8c290a9b01269fc1a697",
+    "url": "/ReactLive/static/media/project-1.e54aabb4.png"
+  },
+  {
+    "revision": "e765ee0f0ab9fee65f60208c6a756344",
+    "url": "/ReactLive/static/media/project-2.e765ee0f.jpg"
+  },
+  {
+    "revision": "ee0cbe37f28a07defaf264cff415484c",
+    "url": "/ReactLive/static/media/project-3.ee0cbe37.jpg"
+  },
+  {
+    "revision": "0336fd79e84f81fd2c44656a726e15f5",
+    "url": "/ReactLive/static/media/project-4.0336fd79.jpg"
+  },
+  {
+    "revision": "8ee32061d8402bda2231411940ed6b20",
+    "url": "/ReactLive/index.html"
+  }
+];
